9f3dc533afdea008e6165caac0f9083430ae3e15
Ingvar Stepanyan <rreverser@google.com>
2022-02-09 16:55:03 +0000

    Link to both features in caniuse

--- ui/index-fallback.js
+++ ui/index-fallback.js
@@ -1,7 +1,7 @@
 document.body.innerHTML = `
 <div class="center-parent">
 	<div class="center">
-		<p>⚠ This browser is not supported: the application relies on <a href="https://caniuse.com/webusb">WebUSB</a> and <a href="https://caniuse.com/import-maps">Import Maps</a>.
+		<p>⚠ This browser is <a href="https://caniuse.com/webusb,import-maps">not supported</a>.
 		<p>Don't know how you got here? Check out the <a href="https://web.dev/porting-libusb-to-webusb/">blog post</a> or the <a href="https://github.com/GoogleChromeLabs/web-gphoto2">repo</a>!
 	</div>
 </div>
