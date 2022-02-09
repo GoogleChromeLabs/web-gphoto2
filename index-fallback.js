1a87c4ced31d84300aea970a6fe91543592f1af0
RReverser <me@rreverser.com>
2022-02-09 17:06:25 +0000

    Deploying to gh-pages from  @ 9f3dc533afdea008e6165caac0f9083430ae3e15 🚀

---
+++ index-fallback.js
@@ -0,0 +1,8 @@
+document.body.innerHTML = `
+<div class="center-parent">
+	<div class="center">
+		<p>⚠ This browser is <a href="https://caniuse.com/webusb,import-maps">not supported</a>.
+		<p>Don't know how you got here? Check out the <a href="https://web.dev/porting-libusb-to-webusb/">blog post</a> or the <a href="https://github.com/GoogleChromeLabs/web-gphoto2">repo</a>!
+	</div>
+</div>
+`;
